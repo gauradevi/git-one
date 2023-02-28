24bef7d174cb55c10bccedbf23eb01751e692786
Gauri <gaurimh199@gmail.com>
2023-02-28 12:45:29 +0530

    Replaced n by m

--- ex1.cpp
+++ ex1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
 
-     int n = 7;
-     for(int i = 0; i<=n; ++i){
+     int m = 7;
+     for(int i = 0; i<=m; ++i){
          cout<<"The value of i is:"<<i<<endl;
          }
          return 0;
