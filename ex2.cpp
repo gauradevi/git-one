cd65d743ad1c5b3ca6b5ad916f70845644fe7ad3
Gauri <gaurimh199@gmail.com>
2023-03-01 12:06:23 +0530

    first commit

---
+++ ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+void fun(int* ptr) { *ptr = 5; }
+ 
+// Driver code
+int main()
+{
+    int x = 9;
+    cout << "value of x before calling fun: " << x << endl;
+    fun(&x);
+ 
+    cout << "value of x after calling fun: " << x << endl;
+    return 0;
+}
+
---
+++ swap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int* x, int *y){
+     int temp = *x;
+     *x = *y;
+     *y = temp;
+     }
+     
+     
+
+int main(){
+
+     int a=2, b =5;
+     cout<<"Value of a and b before swapping"<<endl;
+     
+     swap(&a, &b);
+     
+     cout<<"Value of a and b after swapping"<<endl;
+     }
