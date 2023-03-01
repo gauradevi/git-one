207536783e08d1f5623765d51dff7e03ab785775
Gauri <gaurimh199@gmail.com>
2023-03-01 12:10:06 +0530

    upadted commit

--- ex2.cpp
+++ ex2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
  
-void fun(int* ptr) { *ptr = 5; }
+void function(int* ptr) { *ptr = 5; }
  
 // Driver code
 int main()
 {
     int x = 9;
+    //before swapping
     cout << "value of x before calling fun: " << x << endl;
     fun(&x);
  
+    //after swapping
     cout << "value of x after calling fun: " << x << endl;
     return 0;
 }
